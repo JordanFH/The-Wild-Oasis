1df73c9bcce1aebe95bce2d4f24ee036ff964922
Jordan_FH <jfernandezh19_2@unc.edu.pe>
2023-11-20 19:37:54 -0500

    removed overflow scroll

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -13,8 +13,8 @@ const StyledAppLayout = styled.div`
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
-  overflow: scroll;
-  min-height: 100%;
+  // overflow: scroll;
+  // min-height: 100%;
 `;
 
 const Container = styled.div`
